19429dc2dacab3e811aa521bc1f194c545b1a088
Amelia Lobo <alobo@ece011.ece.local.cmu.edu>
2025-03-09 23:36:05 -0400

    scan code to seven segment done

--- scan_code.sv
+++ scan_code.sv
@@ -5,36 +5,34 @@ module scanCodetoSevenSegment
      input  logic [7:0] scan_code,  
      output logic [6:0] segment);
 
-
-
     always_comb begin
         unique case (scan_code)
-            8'h1C: segment = 7'bxxx_xxxx;       // A
-            8'h32: segment = 7'bxxx_xxxx;       // B
-            8'h21: segment = 7'bxxx_xxxx;       // C
-            8'h23: segment = 7'bxxx_xxxx;       // D
-            8'h24: segment = 7'bxxx_xxxx;       // E
-            8'h2B: segment = 7'bxxx_xxxx;       // F
-            8'h34: segment = 7'bxxx_xxxx;       // G
-            8'h33: segment = 7'bxxx_xxxx;       // H
-            8'h43: segment = 7'bxxx_xxxx;       // I
-            8'h3B: segment = 7'bxxx_xxxx;       // J
-            8'h42: segment = 7'bxxx_xxxx;       // K
-            8'h4B: segment = 7'bxxx_xxxx;       // L
-            8'h3A: segment = 7'bxxx_xxxx;       // M
-            8'h31: segment = 7'bxxx_xxxx;       // N
-            8'h44: segment = 7'bxxx_xxxx;       // O
-            8'h4D: segment = 7'bxxx_xxxx;       // P
-            8'h15: segment = 7'bxxx_xxxx;       // Q
-            8'h2D: segment = 7'bxxx_xxxx;       // R
-            8'h1B: segment = 7'bxxx_xxxx;       // S
-            8'h2C: segment = 7'bxxx_xxxx;       // T
-            8'h3C: segment = 7'bxxx_xxxx;       // U
-            8'h2A: segment = 7'bxxx_xxxx;       // V
-            8'h1D: segment = 7'bxxx_xxxx;       // W
-            8'h22: segment = 7'bxxx_xxxx;       // X
-            8'h35: segment = 7'bxxx_xxxx;       // Y
-            8'h1A: segment = 7'bxxx_xxxx;       // Z
+            8'h1C: segment = 7'b111_0111;       // A
+            8'h32: segment = 7'b001_1111;       // B
+            8'h21: segment = 7'b100_1110;       // C
+            8'h23: segment = 7'b011_1101;       // d
+            8'h24: segment = 7'b100_1111;       // E
+            8'h2B: segment = 7'b100_0111;       // F
+            8'h34: segment = 7'b111_1011;       // g
+            8'h33: segment = 7'b011_0111;       // H
+            8'h43: segment = 7'b011_0000;       // I
+            8'h3B: segment = 7'b011_1000;       // J
+            8'h42: segment = 7'b000_0111;       // K
+            8'h4B: segment = 7'b000_1110;       // L
+            8'h3A: segment = 7'b101_0100;       // M
+            8'h31: segment = 7'b111_0110;       // N
+            8'h44: segment = 7'b111_1110;       // O
+            8'h4D: segment = 7'b110_0111;       // p
+            8'h15: segment = 7'b111_0011;       // q
+            8'h2D: segment = 7'b100_0110;       // r
+            8'h1B: segment = 7'b101_1011;       // S
+            8'h2C: segment = 7'b000_1111;       // t
+            8'h3C: segment = 7'b011_1110;       // U
+            8'h2A: segment = 7'b001_1100;       // v
+            8'h1D: segment = 7'b010_1010;       // W
+            8'h22: segment = 7'b011_0001;       // X
+            8'h35: segment = 7'b011_1011;       // y
+            8'h1A: segment = 7'b110_1101;       // Z
         endcase
     end
 
